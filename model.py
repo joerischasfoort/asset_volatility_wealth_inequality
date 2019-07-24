6b120a94ffe975c40ee2f5513172596f5b66cb47
joerischasfoort <joerischasfoort@gmail.com>
2019-07-24 21:09:16 +0200

    model_eq_update

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 from functions.portfolio_optimization import *
 from functions.helpers import calculate_covariance_matrix, div0, ornstein_uhlenbeck_evolve
+from functions.inequality import gini
 
 
 def volatility_inequality_model(traders, orderbook, parameters, seed=1):
@@ -192,6 +193,121 @@ def volatility_inequality_model2(traders, orderbook, parameters, seed=1):
         # Clear and update order-book history
         orderbook.cleanse_book()
         orderbook.fundamental = fundamental
+
+    return traders, orderbook
+
+
+def volatility_inequality_model_equilibrium(traders, orderbook, parameters, seed=1):
+    """
+    The main model function of distribution model where trader stocks are tracked.
+    :param traders: list of Agent objects
+    :param orderbook: object Order book
+    :param parameters: dictionary of parameters
+    :param seed: integer seed to initialise the random number generators
+    :return: list of simulated Agent objects, object simulated Order book
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    fundamental = [parameters["fundamental_value"]]
+    orderbook.tick_close_price.append(fundamental[-1])
+
+    traders_by_wealth = [t for t in traders]
+
+    equilibrium_found = False
+    tick = parameters['horizon']
+    while not equilibrium_found:
+        tick += 1
+        if tick == parameters['horizon'] + 1:
+            print('Start of simulation ', seed)
+
+        # update money and stocks history for agents
+        for trader in traders:
+            trader.var.money.append(trader.var.money[-1])
+            trader.var.stocks.append(trader.var.stocks[-1])
+            trader.var.wealth.append(trader.var.money[-1] + trader.var.stocks[-1] * orderbook.tick_close_price[-1]) # TODO debug
+
+        # sort the traders by wealth to
+        traders_by_wealth.sort(key=lambda x: x.var.wealth[-1], reverse=True)
+
+        # evolve the fundamental value via random walk process
+        #fundamental.append(max(fundamental[-1] + parameters["std_fundamental"] * np.random.randn(), 0.1))
+        fundamental.append(max(
+            ornstein_uhlenbeck_evolve(parameters["fundamental_value"], fundamental[-1], parameters["std_fundamental"],
+                                      parameters['mean_reversion'], seed), 0.1))
+
+        # allow for multiple trades in one day
+        for turn in range(parameters["trades_per_tick"]):
+            # select random sample of active traders
+            active_traders = random.sample(traders, int((parameters['trader_sample_size'])))
+
+            mid_price = np.mean([orderbook.highest_bid_price, orderbook.lowest_ask_price])
+            fundamental_component = np.log(fundamental[-1] / mid_price)
+
+            orderbook.returns[-1] = (mid_price - orderbook.tick_close_price[-2]) / orderbook.tick_close_price[-2]
+            chartist_component = np.cumsum(orderbook.returns[:-len(orderbook.returns) - 1:-1]
+                                           ) / np.arange(1., float(len(orderbook.returns) + 1))
+
+            for trader in active_traders:
+                # Cancel any active orders
+                if trader.var.active_orders:
+                    for order in trader.var.active_orders:
+                        orderbook.cancel_order(order)
+                    trader.var.active_orders = []
+
+                # Update trader specific expectations
+                noise_component = parameters['std_noise'] * np.random.randn()
+
+                # Expectation formation
+                trader.exp.returns['stocks'] = (
+                        trader.var.weight_fundamentalist[-1] * np.divide(1, float(trader.par.horizon) * parameters["fundamentalist_horizon_multiplier"]) * fundamental_component +
+                        trader.var.weight_chartist[-1] * chartist_component[trader.par.horizon - 1] +
+                        trader.var.weight_random[-1] * noise_component)
+                fcast_price = mid_price * np.exp(trader.exp.returns['stocks'])
+                trader.var.covariance_matrix = calculate_covariance_matrix(orderbook.returns[-trader.par.horizon:],
+                                                                           parameters["std_fundamental"])
+
+                # employ portfolio optimization algo
+                ideal_trader_weights = portfolio_optimization(trader, tick)
+
+                # Determine price and volume
+                trader_price = np.random.normal(fcast_price, trader.par.spread)
+                position_change = (ideal_trader_weights['stocks'] * (trader.var.stocks[-1] * trader_price + trader.var.money[-1])
+                          ) - (trader.var.stocks[-1] * trader_price)
+                volume = int(div0(position_change, trader_price))
+
+                # Trade:
+                if volume > 0:
+                    bid = orderbook.add_bid(trader_price, volume, trader)
+                    trader.var.active_orders.append(bid)
+                elif volume < 0:
+                    ask = orderbook.add_ask(trader_price, -volume, trader)
+                    trader.var.active_orders.append(ask)
+
+            # Match orders in the order-book
+            while True:
+                matched_orders = orderbook.match_orders()
+                if matched_orders is None:
+                    break
+                # execute trade
+                matched_orders[3].owner.sell(matched_orders[1], matched_orders[0] * matched_orders[1])
+                matched_orders[2].owner.buy(matched_orders[1], matched_orders[0] * matched_orders[1])
+
+        # Clear and update order-book history
+        orderbook.cleanse_book()
+        orderbook.fundamental = fundamental
+
+        # determine that a steady state has been reached because no volume for multiple periods & inequality has hit a certain level
+        money = np.array([x.var.money[-1] for x in traders])
+        stocks = np.array([x.var.stocks[-1] for x in traders])
+        wealth = money + (stocks * orderbook.tick_close_price[-1])
+
+        previous_volumes = 0
+        if tick > 211:
+            previous_volumes = sum([sum(x) for x in orderbook.transaction_volumes_history[-10:]])
+
+        if (gini(wealth) > 0.80 and previous_volumes == 0) or tick > 10000:
+            equilibrium_found = True
+            print('Simulation ends in tick ', tick, ' with Gini ', gini(wealth))
 
     return traders, orderbook
 
--- simulate.py
+++ simulate.py
@@ -5,27 +5,29 @@ import time
 start_time = time.time()
 
 # # 1 setup parameters
-parameters = {"fundamental_value": 166,
-          "trader_sample_size": 10, "n_traders": 1000,
-          "ticks": 500, "std_fundamental": 180.0,#0.530163128919286,
-          "std_noise": 0.10696588473846724,
-          "w_random": 0.1,
-          "init_stocks": 50, "base_risk_aversion": 1.0,
-          'spread_max': 0.004087, "horizon": 200,
-          "fundamentalist_horizon_multiplier": 0.2,
-          "mean_reversion": 1.2,
-          # fixed / not modelled parameters
-          "strat_share_chartists": 0.0,
-          "mutation_intensity": 0.0,
-          "average_learning_ability": 0.0,
-          "trades_per_tick": 1
-}
+parameters = {'trader_sample_size': 10,
+ 'n_traders': 1000,
+ 'init_stocks': 81,
+ 'ticks': 606,
+ 'fundamental_value': 1112.2356754564078,
+ 'std_fundamental': 0.036106530849401956,
+ 'base_risk_aversion': 0.7,
+ 'spread_max': 0.004087,
+ 'horizon': 212,
+ 'std_noise': 0.01,
+ 'w_random': 1.0,
+ 'mean_reversion': 0.0,
+ 'fundamentalist_horizon_multiplier': 1.0,
+ 'strat_share_chartists': 0.0,
+ 'mutation_intensity': 0.0,
+ 'average_learning_ability': 0.0,
+ 'trades_per_tick': 1}
 
 # 2 initialise model objects
 traders, orderbook = init_objects_distr(parameters, seed=0)
 
 # 3 simulate model
-traders, orderbook = volatility_inequality_model_reset_wealth(traders, orderbook, parameters, seed=0)
+traders, orderbook = volatility_inequality_model_equilibrium(traders, orderbook, parameters, seed=0)
 
 
 print("The simulations took", time.time() - start_time, "to run")
